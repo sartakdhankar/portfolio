79afabfcabb269c423b499f602ca7d0913541712
sartakdhankar <145408777+sartakdhankar@users.noreply.github.com>
2025-08-13 22:16:09 +0530

    Updated Commits

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import '../styles/Header.css';
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+
+
+const Header = () => {
+  return (
+    <header className="header">
+        <div className="logo">
+      <h1 className="header-title">Sartak's Portfolio</h1>
+      </div>
+      <nav className="header-nav">
+        <ul className="nav-link">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#education">Education</a></li>
+            <li><a href="#clubs">College Societies/Clubs</a></li>
+            <li><a href="#contact">Contact</a></li>
+
+        </ul>
+          <div className="header-right">
+            <a href="https://www.linkedin.com/in/yourprofile" target="_blank">
+            <FaLinkedin className="header-icon" />
+            </a>
+            <a href="https://github.com/yourusername" target="_blank">
+            <FaGithub className="header-icon" />
+            </a>
+            <a href="https://leetcode.com/yourusername" target="_blank">
+            <SiLeetcode className="header-icon" />
+            </a>
+            </div>
+
+      </nav>
+    </header>
+  );
+}
+export default Header;
