4cb0882673bddc8287aae1f063b4f909677f91b2
sartakdhankar <145408777+sartakdhankar@users.noreply.github.com>
2025-08-13 22:23:01 +0530

    Updated commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Header from "./components/Header.jsx";
-import Hero from "./components/Hero.jsx"; // example
-import About from "./components/About.jsx"; // example
+// import Hero from "./components/Hero.jsx"; // example
+// import About from "./components/About.jsx"; // example
 import './styles/globals.css'; // global styles
 
 function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <About />
+      {/* <Hero />
+      <About /> */}
       {/* other sections */}
     </>
   );
